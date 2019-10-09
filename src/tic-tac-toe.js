69ad997ff379365696f6b6be8646fd3378e276df
Yuriy17 <yuriy_sh@zoho.com>
2019-10-09 16:27:06 +0300

    tictactoe:feat: realize base structure

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,18 +1,42 @@
 class TicTacToe {
     constructor() {
+        this.xy=[
+            [null,null,null],
+            [null,null,null],
+            [null,null,null]
+        ];
+        this.first_player;
+        this.second_player;
+        this.current_player;
 
     }
 
     getCurrentPlayerSymbol() {
-
+        if(!this.first_player&&!this.second_player){
+            this.first_player='x';
+            this.second_player='o';
+            this.current_player='x';
+        }
+        return this.current_player;
+        
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        this.xy[rowIndex,columnIndex]=this.current_player;
+        this.current_player=this.current_player==this.first_player?
+                            this.second_player:
+                            this.first_player;
     }
 
     isFinished() {
-
+        this.xy.forEach(function(stroke){
+            stroke.forEach(function(item) {
+                if(item==null){
+                    return false;
+                }
+            })
+        });
+        return true;
     }
 
     getWinner() {
