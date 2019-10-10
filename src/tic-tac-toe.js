5ebba4c5e7879860227c29554d7c15838807f6ec
Yuriy17 <yuriy_sh@zoho.com>
2019-10-10 21:39:19 +0300

    tic-tac-toe/feat: complete task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -25,22 +25,40 @@ class TicTacToe {
     }
 
     isFinished() {
-        this.xo.forEach(function (stroke) {
-            stroke.forEach(function (item) {
-                if (item == null) {
-                    return false;
-                }
-            })
-        });
-        return true;
+        return this.isDraw()||this.getWinner()!==null?true:false;
     }
 
     getWinner() {
-
+        if ((this.xo[0][0] == 'x' && this.xo[0][1] == 'x' && this.xo[0][2] == 'x') ||
+            (this.xo[1][0] == 'x' && this.xo[1][1] == 'x' && this.xo[1][2] == 'x') ||
+            (this.xo[2][0] == 'x' && this.xo[2][1] == 'x' && this.xo[2][2] == 'x') ||
+            (this.xo[0][0] == 'x' && this.xo[1][0] == 'x' && this.xo[2][0] == 'x') ||
+            (this.xo[0][1] == 'x' && this.xo[1][1] == 'x' && this.xo[2][1] == 'x') ||
+            (this.xo[0][2] == 'x' && this.xo[1][2] == 'x' && this.xo[2][2] == 'x') ||
+            (this.xo[0][0] == 'x' && this.xo[1][1] == 'x' && this.xo[2][2] == 'x') ||
+            (this.xo[0][2] == 'x' && this.xo[1][1] == 'x' && this.xo[2][0] == 'x')){
+            return 'x';
+        }else if((this.xo[0][0] == 'o' && this.xo[0][1] == 'o' && this.xo[0][2] == 'o') ||
+            (this.xo[1][0] == 'o' && this.xo[1][1] == 'o' && this.xo[1][2] == 'o') ||
+            (this.xo[2][0] == 'o' && this.xo[2][1] == 'o' && this.xo[2][2] == 'o') ||
+            (this.xo[0][0] == 'o' && this.xo[1][0] == 'o' && this.xo[2][0] == 'o') ||
+            (this.xo[0][1] == 'o' && this.xo[1][1] == 'o' && this.xo[2][1] == 'o') ||
+            (this.xo[0][2] == 'o' && this.xo[1][2] == 'o' && this.xo[2][2] == 'o') ||
+            (this.xo[0][0] == 'o' && this.xo[1][1] == 'o' && this.xo[2][2] == 'o') ||
+            (this.xo[0][2] == 'o' && this.xo[1][1] == 'o' && this.xo[2][0] == 'o')){
+            return 'o';
+        }else {return null;}
     }
 
     noMoreTurns() {
-
+        for (let stroke of this.xo){
+            for(let item of stroke){
+                if (item === null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     isDraw() {
@@ -51,20 +69,28 @@ class TicTacToe {
                 }
             }
         }
-/*         this.xo.forEach((stroke) => {
-            stroke.forEach((item) => {
-                if (item == null) {
-                    return false;
-                }
-            })
-        }); */
-        return true;
+        return (!((this.xo[0][0] == 'x' && this.xo[0][1] == 'x' && this.xo[0][2] == 'x') ||
+                (this.xo[1][0] == 'x' && this.xo[1][1] == 'x' && this.xo[1][2] == 'x') ||
+                (this.xo[2][0] == 'x' && this.xo[2][1] == 'x' && this.xo[2][2] == 'x') ||
+                (this.xo[0][0] == 'x' && this.xo[1][0] == 'x' && this.xo[2][0] == 'x') ||
+                (this.xo[0][1] == 'x' && this.xo[1][1] == 'x' && this.xo[2][1] == 'x') ||
+                (this.xo[0][2] == 'x' && this.xo[1][2] == 'x' && this.xo[2][2] == 'x') ||
+                (this.xo[0][0] == 'x' && this.xo[1][1] == 'x' && this.xo[2][2] == 'x') ||
+                (this.xo[0][2] == 'x' && this.xo[1][1] == 'x' && this.xo[2][0] == 'x'))&&
+                !((this.xo[0][0] == 'o' && this.xo[0][1] == 'o' && this.xo[0][2] == 'o') ||
+                (this.xo[1][0] == 'o' && this.xo[1][1] == 'o' && this.xo[1][2] == 'o') ||
+                (this.xo[2][0] == 'o' && this.xo[2][1] == 'o' && this.xo[2][2] == 'o') ||
+                (this.xo[0][0] == 'o' && this.xo[1][0] == 'o' && this.xo[2][0] == 'o') ||
+                (this.xo[0][1] == 'o' && this.xo[1][1] == 'o' && this.xo[2][1] == 'o') ||
+                (this.xo[0][2] == 'o' && this.xo[1][2] == 'o' && this.xo[2][2] == 'o') ||
+                (this.xo[0][0] == 'o' && this.xo[1][1] == 'o' && this.xo[2][2] == 'o') ||
+                (this.xo[0][2] == 'o' && this.xo[1][1] == 'o' && this.xo[2][0] == 'o'))
+                )?true:false;
+        
     }
 
     getFieldValue(rowIndex, colIndex) {
-        
         return this.xo[rowIndex][colIndex];
-
     }
 }
 
